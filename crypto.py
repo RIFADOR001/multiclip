e0d1f5d4f5962c76146cd1a4ca78f2550fd98aaa
Alejandro Mendez Rojas <amr@Alejandros-MacBook-Pro.local>
2023-08-10 11:26:25 +0900

    First commit of the project

---
+++ crypto.py
@@ -0,0 +1,49 @@
+import string
+import random
+
+
+def encode_text(text, passcode=None):
+    if passcode is None:
+        return text
+    random.seed(a=passcode)
+    characters = " " + string.punctuation + string.digits + string.ascii_letters
+    chars_list = list(characters)
+    key_list = chars_list.copy()
+    random.shuffle(key_list)
+    aux_list = []
+    for i in text:
+        if i in key_list:
+            char_index = key_list.index(i)
+            aux_list.append(chars_list[char_index])
+        else:
+            aux_list.append(i)
+    return "".join(aux_list)
+
+
+def decode_text(text, passcode=None):
+    if passcode is None:
+        return text
+    random.seed(a=passcode)
+    characters = " " + string.punctuation + string.digits + string.ascii_letters
+    chars_list = list(characters)
+    key_list = chars_list.copy()
+    random.shuffle(key_list)
+    aux_list = []
+    for i in text:
+        if i in chars_list:
+            char_index = chars_list.index(i)
+            aux_list.append(key_list[char_index])
+        else:
+            aux_list.append(i)
+    return "".join(aux_list)
+
+
+if __name__ == "__main__":
+    txt = "patito"
+    code = "yo"
+    encoded_text = encode_text(txt, code)
+    decoded_text = decode_text(encoded_text, code)
+    print(txt)
+    print(encoded_text)
+    print(decoded_text)
+
